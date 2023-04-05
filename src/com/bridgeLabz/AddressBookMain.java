fcf76b85caaf37de6a1be8d19fbda53968568194
Vamshi77reddy <vamshireddy6563@gmail.com>
2023-04-05 13:02:20 +0530

    addedNewcontact

--- src/com/bridgeLabz/AddressBookMain.java
+++ src/com/bridgeLabz/AddressBookMain.java
@@ -1,7 +1,25 @@
 package com.bridgeLabz;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to AddressBook");
+        AddressBook addressBook = new AddressBook();
+
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.println("  ::>>>Welcome AdressBook<<<::  ");
+
+            System.out.println("1.Add New Contcat");
+            System.out.println("2.View All Contacts");
+            int input = sc.nextInt();
+            switch (input){
+                case 1 -> {
+                    addressBook.AddContact();
+                }
+                case 2 -> {
+                    addressBook.display();
+                }
+        }}
     }
 }
